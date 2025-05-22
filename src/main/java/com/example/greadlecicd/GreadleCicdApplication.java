b3e2339fd320c9a7a1fb4b0c3a0afb0fd4670d7f
Seungwon-Choi <bestwondev@gmail.com>
2025-05-22 09:32:43 +0900

    initial commit

---
+++ src/main/java/com/example/greadlecicd/GreadleCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.greadlecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreadleCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreadleCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/greadlecicd/GreadleCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greadlecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreadleCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
